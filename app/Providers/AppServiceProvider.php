ddff8b44aec940d4cca31099d61a65517355e98d
sjahn <asj214@naver.com>
2023-03-08 01:39:01 +0900

    sql query log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use DB;
+use Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +22,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (env('APP_DEBUG', false)) {
+            DB::listen(function($query){
+
+                foreach ($query->bindings as $i => $bind) {
+                    if($bind instanceof \DateTime){
+                        $query->bindings[$i] = $bind->format('\'Y-m-d H:i:s\'');
+                    } else {
+                        if(is_string($bind)){
+                            $query->bindings[$i] = "'$bind'";
+                        }
+                    }
+                }
+
+                $result = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+                $result = vsprintf($result, $query->bindings);
+                Log::debug("(".$query->time."s) ".$result);
+            });
+        }
     }
 }
